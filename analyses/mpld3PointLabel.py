3bd658deb1aa1f38bbdae15e7bde3456594e8f3a
Sven Kreiss <sk@svenkreiss.com>
2014-05-30 20:24:30 -0400

    minor

--- analyses/mpld3PointLabel.py
+++ analyses/mpld3PointLabel.py
@@ -8,14 +8,12 @@ import matplotlib.pyplot as plt, mpld3
 import databench
 
 
-
 signals = databench.Signals('mpld3PointLabel')
 
 @signals.on('connect')
 def onconnect():
 	fig, ax = plt.subplots()
-	points = ax.scatter(np.random.rand(40), np.random.rand(40),
-	                    s=300, alpha=0.3)
+	points = ax.scatter(np.random.rand(40), np.random.rand(40), s=300, alpha=0.3)
 
 	labels = ["Point {0}".format(i) for i in range(40)]
 	tooltip = mpld3.plugins.PointLabelTooltip(points, labels)
