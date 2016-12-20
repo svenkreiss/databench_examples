6dd4ca4a288f5cd35d63276d0ac03669d3aef5a7
Sven Kreiss <me@svenkreiss.com>
2016-12-19 22:39:09 -0500

    update

--- analyses/redissub/analysis.js
+++ analyses/redissub/analysis.js
@@ -5,17 +5,17 @@ const databench = new Databench.Connection();
 Databench.ui.wire(databench);
 
 // listen for data from backend
-let inputData = {};
-let lastUpdate = {};
+const inputData = {};
+const lastUpdate = {};
 databench.on('status', (json) => {
   const srcName = json.split('=')[0].trim();
   inputData[srcName] = json.split('=')[1];
   lastUpdate[srcName] = Date.now();
 
   // remove old values from data
-  for (k in lastUpdate) {
+  Object.keys(lastUpdate).forEach((k) => {
     if (Date.now() - lastUpdate[k] > 1500) delete inputData[k];
-  }
+  });
 
   // visualize
   console.log(inputData);
